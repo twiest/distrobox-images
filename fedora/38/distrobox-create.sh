d4bdf1a2cd8dbdf9a69761f9592de4efa893787d
Thomas Wiest <th.wiest@celonis.com>
2023-08-06 22:11:47 -0400

    Add Fedora 38 distrobox container.

---
+++ fedora/38/distrobox-create.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eou pipefail
+
+if [ $# -ne 1 ]; then
+  echo
+  echo "  Usage: $(basename $0) <permission_domain_for_box>"
+  echo
+  echo "Example: $(basename $0) financial"
+  echo
+  exit 10
+fi
+
+cd $(dirname $(readlink -f $0))
+
+img_name=distrobox-$(basename $(dirname $PWD))
+img_ver=$(basename $PWD)
+date_stamp=$(date +%Y-%m-%d)
+
+
+distrobox_dir="$HOME/distrobox"
+home_dir="$distrobox_dir/$1"
+
+
+mkdir -p "$home_dir"
+
+#cp /etc/skel/.bashrc "$home_dir"
+chown -R twiest:twiest "$home_dir"
+
+img_name=distrobox-$(basename $(dirname $PWD))
+img_ver=$(basename $PWD)
+date_stamp=$(date +%Y-%m-%d)
+
+distrobox create --name "$1" --home "$home_dir" --image "${img_name}:${img_ver}"
