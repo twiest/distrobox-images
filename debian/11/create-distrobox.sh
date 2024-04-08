d3e2ec178d83492dc0930714aea2647f304ec048
Thomas Wiest <twg-github@wiestfamily.org>
2024-04-07 22:08:19 -0400

    Add --unshare-* options to all distrobox create scripts to enhance default security.
    NOTE: --unshare-ipc is not included as it causes brave-browser to crash.

--- debian/11/create-distrobox.sh
+++ debian/11/create-distrobox.sh
@@ -31,4 +31,4 @@ img_name=distrobox-$(basename $(dirname $PWD))
 img_ver=$(basename $PWD)
 date_stamp=$(date +%Y-%m-%d)
 
-distrobox create --name "$1" --home "$home_dir" --nvidia --image "${img_name}:${img_ver}"
+distrobox create --nvidia --unshare-process --unshare-netns --unshare-groups --unshare-devsys --name "$1" --home "$home_dir" --image "${img_name}:${img_ver}"
