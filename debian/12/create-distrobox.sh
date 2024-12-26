6724a857177a6415283b1ccc16a167368a4fb6fe
Thomas Wiest <twg-github@wiestfamily.org>
2024-12-26 15:27:17 -0500

    debian/12/create-distrobox.sh: make it zone aware.

--- debian/12/create-distrobox.sh
+++ debian/12/create-distrobox.sh
@@ -13,22 +13,35 @@ fi
 
 cd $(dirname $(readlink -f $0))
 
+zone=$1
 img_name=distrobox-$(basename $(dirname $PWD))
 img_ver=$(basename $PWD)
 date_stamp=$(date +%Y-%m-%d)
 
 
 distrobox_dir="$HOME/distrobox"
-home_dir="$distrobox_dir/$1"
-
-
-mkdir -p "$home_dir"
-
-#cp /etc/skel/.bashrc "$home_dir"
-chown -R twiest:twiest "$home_dir"
+home_dir="$distrobox_dir/$zone"
 
 img_name=ghcr.io/twiest/distrobox-$(basename $(dirname $PWD))
 img_ver=$(basename $PWD)
 date_stamp=$(date +%Y-%m-%d)
 
+has_zfs_dir=false
+zfs_fs=
+if [ -d "${home_dir}/.zfs" ]; then
+  has_zfs_dir=true
+  zfs_fs="ssd/zones/${zone}-enc"
+  echo -n "Disabling .zfs dir for [$zfs_fs]... "
+  sudo zfs set snapdir=hidden "$zfs_fs"
+  echo "Done."
+fi
+
+mkdir -p "$home_dir"
+sudo chown -R twiest:twiest "$home_dir"
 distrobox create --nvidia --unshare-process --unshare-groups --unshare-devsys --name "$1" --home "$home_dir" --image "${img_name}:${img_ver}"
+
+if [ "$has_zfs_dir" == "true" ]; then
+  echo -n "Resetting .zfs dir to inheritted for [$zfs_fs]... "
+  sudo zfs inherit snapdir "$zfs_fs"
+  echo "Done."
+fi
